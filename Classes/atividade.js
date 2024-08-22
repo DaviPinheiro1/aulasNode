5bc561bd0c84a9ad9306dd37c655398c86167ba6
DaviPinheiro1 <dgap.snf23@uea.edu.br>
2024-08-22 09:24:36 -0400

    arquivos

--- Classes/atividade.js
+++ Classes/atividade.js
@@ -9,16 +9,17 @@ function salvar(){
     }
 }
 
-function salvar1(){
-    const graus = document.querySelectorAll('input[name="Transporte"]');
-    let valorselecionado;
-    for(const grau of graus){
-        if(grau.checked){
-            valorselecionado = grau.value;
-            return valorselecionado;
-        }
-    }
+function salvar1() {
+    const checkboxes = document.querySelectorAll('input[name="Transporte"]:checked');
+    let selecionados = [];
+
+    checkboxes.forEach((checkbox) => {
+        selecionados.push(checkbox.value);
+    });
+
+    return selecionados;
 }
+
 function botao(){
     const cadastro = new Object();
         cadastro.nome= document.getElementById("Nome").value,
